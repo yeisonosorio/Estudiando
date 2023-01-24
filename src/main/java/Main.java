b919d1f49ee2d77d96a04dfd8af68d578a535f5c
yeisonosorio <yeisonferneyob@ufps.edu.co>
2023-01-24 16:06:31 -0500

    repasando listas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,45 @@
+import java.util.*;
+
 public class Main {
 
 
     public static void main(String[] args) {
+
+        /**
+        //creamos la lista
+        List<Persona> lista = new ArrayList<Persona>();
+        //agg la persona con el objeto
+        lista.add(new Persona(1, "pedro", 2));
+        lista.add(new Persona(2, "juan", 3));
+        lista.add(new Persona(3, "yeison", 4));
+        lista.add(new Persona(4, "yuli", 5));
+
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println("prueba:" + lista.get(2).getNombre());
+        }
+
+        System.out.println("----------------------------separando-----------------");
+
+
+        for (Persona perso : lista) {
+            System.out.println("prueba" + perso.getNombre());
+
+        }
+
+         //probando las listas
+         */
+
+
+
+
+
+
+
+
+
     }
+
+
 }
+
