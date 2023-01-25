84da9d33e09c8e3798dc7c9b46000cfe948b07c9
yeisonosorio <yeisonferneyob@ufps.edu.co>
2023-01-24 21:28:31 -0500

    repasando array linkedlist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,37 +6,73 @@ public class Main {
     public static void main(String[] args) {
 
         /**
-        //creamos la lista
-        List<Persona> lista = new ArrayList<Persona>();
-        //agg la persona con el objeto
-        lista.add(new Persona(1, "pedro", 2));
-        lista.add(new Persona(2, "juan", 3));
-        lista.add(new Persona(3, "yeison", 4));
-        lista.add(new Persona(4, "yuli", 5));
+         //creamos la lista
+         List<Persona> lista = new ArrayList<Persona>();
+         //agg la persona con el objeto
+         lista.add(new Persona(1, "pedro", 2));
+         lista.add(new Persona(2, "juan", 3));
+         lista.add(new Persona(3, "yeison", 4));
+         lista.add(new Persona(4, "yuli", 5));
 
 
-        for (int i = 0; i < lista.size(); i++) {
-            System.out.println("prueba:" + lista.get(2).getNombre());
-        }
+         for (int i = 0; i < lista.size(); i++) {
+         System.out.println("prueba:" + lista.get(2).getNombre());
+         }
 
-        System.out.println("----------------------------separando-----------------");
+         System.out.println("----------------------------separando-----------------");
 
 
-        for (Persona perso : lista) {
-            System.out.println("prueba" + perso.getNombre());
+         for (Persona perso : lista) {
+         System.out.println("prueba" + perso.getNombre());
 
-        }
+         }
 
          //probando las listas
          */
 
+        /**
+         * importamos la linkendList
+
+         List<Persona> lista = new LinkedList<>();
+
+
+         //agg la persona con el objeto
+         lista.add(new Persona(1, "pedro", 2));
+         lista.add(new Persona(2, "juan", 3));
+         lista.add(new Persona(3, "yeison", 4));
+         lista.add(new Persona(4, "yuli", 5));
+
+         //agragar al principio
+
+         lista.add(0, new Persona(1, "juan probando", 12));
+
+
+         System.out.println("------------------se recorre con el forEach-----------------");
+
+
+         for (Persona perso : lista) {
+         System.out.println("prueba" + " :  " + perso.getNombre());
+
+         }
 
 
 
+         */
 
 
+        //declarar un array
+        String[] personas = new String[4];
+        //lo inicializamos
 
+        personas[0] = "juan";
+        personas[1] = "pedro";
+        personas[2] = "jului";
+        personas[3] = "yuli";
 
+        for (int i = 0; i < personas.length; i++) {
+            System.out.println("prueba: " + personas[i]);
+
+        }
 
     }
 
